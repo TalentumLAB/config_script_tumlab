cc027fe13eb7c601c511f9db5a23cbc53f5dd3b5
Jose Ramiro Ramirez Vargas <especialista.servidores@talentum.edu.co>
2023-09-12 22:25:51 -0500

    Add new config to quindio servers

---
+++ quindio_srv/config_service_syncthing.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+path_xml="/root/.config/syncthing/config.xml"
+name_label=$(hostname)
+
+checkExitsFile() {
+    file="$1"
+    retval=""
+    if [[ -f "$file" ]]; then
+        retval="true"
+    else
+        retval="false"
+    fi
+    echo $retval
+}
+
+exist_xml_file=$(checkExitsFile $path_xml)
+
+if [[ $exist_xml_file == 'true' ]]; then
+    sudo rm -r /root/.config/syncthing
+    sudo systemctl stop syncthing.service
+    sudo rm /lib/systemd/system/syncthing.service
+fi
+
+exist_file=$(checkExitsFile syncthing-linux-amd64-v1.22.2/syncthing)
+if [[ $exist_file == 'false' ]]; then
+    wget https://github.com/syncthing/syncthing/releases/download/v1.22.2/syncthing-linux-amd64-v1.22.2.tar.gz
+    tar -xf syncthing-linux-amd64-v1.22.2.tar.gz
+    rm syncthing-linux-amd64-v1.22.2.tar.gz
+fi
+
+example_service_file="./syncthing.service.example"
+cat $example_service_file > /lib/systemd/system/syncthing.service
+
+sudo systemctl enable syncthing
+
+sudo systemctl start syncthing 
+
+sleep 3
+id_tumlab=$(xmlstarlet sel -t -c "/configuration/folder/device[1]" "$path_xml" |grep id | awk -F '"' '{print $2}')
+
+echo "Digite el numero del municipio. Balboa=1, Miranda=2, Cajibio=3, B/Aires=4, sipi=5, patia=6, p/leguizamo=7, valle=8, quindio=9 "
+read -r id_municipio
+
+case "${id_municipio}" in
+1)
+    id_server="77M2ZQO-B3BIAV5-DPDVVSL-5XWWWIC-IWZHVGW-N5RQ6XP-UJN445X-EZKSDAZ"
+    ;;
+2)
+    id_server="6PEY5Y2-YMQWHI5-AMII2NP-DO6LFTU-77KAJVS-NKPE5CP-B52UCAM-ZXN2IAF"
+    ;;
+3)
+    id_server="726VG2X-B7CECSS-JWHJITX-IQMO6FD-YFMW6VW-UNVLW4R-R5BVHMO-Q5R6UAT"
+    ;;
+4)
+    id_server="B27ZIO6-42QREVU-TPDK756-6PETO76-TQHEF5G-JLQKX7B-DBJCVQI-WVDKYAN"
+    ;;
+5)
+    id_server="YP3TXCL-5KECCHI-FUB2EAT-XFC7FRT-CDU32EU-O3JEDTR-RTJTSM7-XR33AA5"
+    ;;
+6)
+    id_server="NYOMHPF-6O5LJZW-Q3KOL2A-QVAOGRS-XTXRZEG-CVQWYXJ-UZJ6KKY-NARXWA4"
+    ;;
+7)
+    id_server="QCISH6N-KPI524N-SM7G43S-MJ4T7QK-BGGHYV4-PCLNL2N-HLAIBSV-ABFH5AL"
+    ;;
+8)
+    id_server="PC4H5G3-GLV2AYL-FEOJ6YE-BLIR5SF-YKTWOWC-MZXQC3I-7BSJLFJ-NYLLMQC"
+    ;;
+9)
+    id_server="ZKILJ7P-YI7QCMG-6JPZHHU-JLHB6EY-AB5XXHA-LTSGLXM-53W5QWF-3OPEUAJ"
+    ;;
+*)
+    echo "Valor ingresado incorrecto"
+    rm /lib/systemd/system/syncthing.service
+    exit 1
+    ;;
+esac
+
+echo "$id_tumlab"
+echo "$id_server"
+(xmlstarlet ed --subnode "/configuration" --type elem -n newsubnode "$path_xml" | xmlstarlet ed --insert //newsubnode --type attr -n id -v "$id_tumlab" | xmlstarlet ed --insert //newsubnode --type attr -n label -v "$name_label" | xmlstarlet ed --insert //newsubnode --type attr -n path -v /tumlab/syncthing/ | xmlstarlet ed --insert //newsubnode --type attr -n type -v sendreceive | xmlstarlet ed --insert //newsubnode --type attr -n rescanIntervalS -v 3600 | xmlstarlet ed --insert //newsubnode --type attr -n fsWatcherEnabled -v true | xmlstarlet ed --insert //newsubnode --type attr -n fsWatcherDelayS -v 10 | xmlstarlet ed --insert //newsubnode --type attr -n ignorePerms -v false | xmlstarlet ed --insert //newsubnode --type attr -n autoNormalize -v true | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n filesystemType -v "basic" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n device -v "" | xmlstarlet ed --insert //newsubnode/device --type attr -n id -v JUPCPPO-ROYV4PB-PSSU7WS-5JSJ57H-GFTECV5-KG45DBU-HQDGY66-6UA5MAQ | xmlstarlet ed --insert //newsubnode/device --type attr -n introducedBy -v '' | xmlstarlet ed --subnode "/configuration/newsubnode/device[@introducedBy='']" --type elem -n encryptionPassword -v "" | xmlstarlet ed --subnode "/configuration/newsubnode[@ignorePerms='false']" --type elem -n devices -v "" | xmlstarlet ed --insert //newsubnode/devices --type attr -n id -v "$id_server" | xmlstarlet ed --insert //newsubnode/devices --type attr -n introducedBy -v '' | xmlstarlet ed --subnode "/configuration/newsubnode/devices[@introducedBy='']" --type elem -n encryptionPassword -v "" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n minDiskFree -v "1" | xmlstarlet ed --insert //newsubnode/minDiskFree --type attr -n unit -v '%' | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n versioning -v "" | xmlstarlet ed --subnode "/configuration/newsubnode/versioning" --type elem -n cleanupIntervalS -v "3600" | xmlstarlet ed --subnode "/configuration/newsubnode/versioning" --type elem -n fsPath -v "" | xmlstarlet ed --subnode "/configuration/newsubnode/versioning" --type elem -n fsType -v "basic" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n versioning -v "" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n copiers -v "0" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n pullerMaxPendingKiB -v "0" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n hashers -v "0" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n order -v "random" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n ignoreDelete -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n scanProgressIntervalS -v "0" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n pullerPauseS -v "0" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n maxConflicts -v "10" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n disableSparseFiles -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n disableTempIndexes -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n paused -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n weakHashThresholdPct -v "25" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n markerName -v ".stfolder" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n copyOwnershipFromParent -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n modTimeWindowS -v "0" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n maxConcurrentWrites -v "2" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n disableFsync -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n blockPullOrder -v "standard" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n copyRangeMethod -v "standard" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n caseSensitiveFS -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n junctionsAsDirs -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n syncOwnership -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n sendOwnership -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n syncXattrs -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@autoNormalize='true']" --type elem -n xattrFilter -v "" | xmlstarlet ed --subnode "/configuration/newsubnode/xattrFilter" --type elem -n maxSingleEntrySize -v "1024" | xmlstarlet ed --subnode "/configuration/newsubnode/xattrFilter" --type elem -n maxTotalSize -v "4096" | sed -r 's/newsubnode/folder/g' | sed -r 's/devices/device/g') > temp.xml && rm "$path_xml" && cat temp.xml > "$path_xml" && rm temp.xml
+xmlstarlet edit --update '/configuration/gui/address' --value '0.0.0.0:8384' "$path_xml" > temp.xml && rm "$path_xml" && cat temp.xml > "$path_xml" && rm temp.xml
+(xmlstarlet ed --subnode "/configuration" --type elem -n newsubnode "$path_xml" | xmlstarlet ed --insert //newsubnode --type attr -n id -v "$id_server" | xmlstarlet ed --insert //newsubnode --type attr -n name -v tumlab-cloud | xmlstarlet ed --insert //newsubnode --type attr -n compression -v metadata | xmlstarlet ed --insert //newsubnode --type attr -n introducer -v false | xmlstarlet ed --insert //newsubnode --type attr -n skipIntroductionRemovals -v false | xmlstarlet ed --insert //newsubnode --type attr -n introducedBy -v '' | xmlstarlet ed --subnode "/configuration/newsubnode[@introducedBy='']" --type elem -n address -v "dynamic" | xmlstarlet ed --subnode "/configuration/newsubnode[@introducedBy='']" --type elem -n paused -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@introducedBy='']" --type elem -n autoAcceptFolders -v "false" | xmlstarlet ed --subnode "/configuration/newsubnode[@introducedBy='']" --type elem -n maxSendKbps -v "0" | xmlstarlet ed --subnode "/configuration/newsubnode[@introducedBy='']" --type elem -n maxRecvKbps -v "0" | xmlstarlet ed --subnode "/configuration/newsubnode[@introducedBy='']" --type elem -n maxRequestKiB -v "0" | xmlstarlet ed --subnode "/configuration/newsubnode[@introducedBy='']" --type elem -n untrusted -v "false" | xmlstarlet ed -O --subnode "/configuration/newsubnode[@introducedBy='']" --type elem -n remoteGUIPort -v "0" | sed -r 's/newsubnode/device/g') > temp.xml && rm "$path_xml" && cat temp.xml > "$path_xml" && rm temp.xml
+cat "$path_xml"
+
+sudo systemctl restart syncthing 
---
+++ quindio_srv/config_tumlab_param.sh
@@ -0,0 +1,116 @@
+#!/bin/bash 
+
+echo "------------Parametrizacion de informacion del tumlab------------ "
+
+path_json="/syncthing/agent.config"
+echo "$path_json"
+#path_config_promtail="/tumlab/apps/promtail/config-promtail.yml"
+
+# ramdom_licence_number=$(echo $RANDOM | md5sum | head -c 8)
+# echo "ramdom_licece_number= $ramdom_licence_number"
+
+check_number(){
+    value="$1"
+    retval=""
+    if [[ $value -lt 0 ]]; then
+        retval="false"
+    else
+        retval="true"
+    fi
+    echo "$retval"
+}
+
+add_parameters(){
+    parameter_name="$1"
+    parameter="$2"
+    array_position="$3"
+    jq '.tumlab.information['"$array_position"'].'"$parameter_name"''' ='"$parameter"'' $path_json > tmp.json && rm $path_json && jq '.' tmp.json > $path_json && rm tmp.json
+    
+}
+
+echo "Digite el numero de lote: "
+read -r id_batch
+if_value_is_avaiable=$(check_number "$id_batch")
+if [[ $if_value_is_avaiable == 'true' ]]; then
+    add_parameters id_batch "$id_batch" 0
+else
+    echo "wrong entered value: $id_batch "  
+fi
+
+echo "Digite el numero del projecto: "
+read -r id_project
+if_value_is_avaiable=$(check_number "$id_project")
+if [[ $if_value_is_avaiable == 'true' ]]; then
+    add_parameters id_project "$id_project" 1
+else
+    echo "wrong entered value: $id_project"
+fi
+
+echo "Digite el numero del departamento: "
+read -r id_deparment
+if_value_is_avaiable=$(check_number "$id_deparment")
+if [[ $if_value_is_avaiable == 'true' ]]; then
+    add_parameters id_deparment "$id_deparment" 2
+else
+    echo "wrong entered value: $id_deparment"
+fi
+
+echo "Digite el numero de la ciudad: "
+read -r id_town
+if_value_is_avaiable=$(check_number "$id_town")
+if [[ $if_value_is_avaiable == 'true' ]]; then
+    add_parameters id_town "$id_town" 3
+else
+    echo "wrong entered value: $id_town"
+fi
+
+echo "Digite el numero de la institucion: "
+read -r id_institution
+if_value_is_avaiable=$(check_number "$id_institution")
+if [[ $if_value_is_avaiable == 'true' ]]; then
+    add_parameters id_institution "$id_institution" 4
+else
+    echo "wrong entered value: $id_institution"
+fi
+
+echo "Digite el numero de la sucursal: "
+read -r id_branch
+if_value_is_avaiable=$(check_number "$id_branch")
+if [[ $if_value_is_avaiable == 'true' ]]; then
+    add_parameters id_branch "$id_branch" 5
+else
+    echo "wrong entered value: $id_branch"
+fi
+
+#interface="$(ls /sys/class/net/ | grep wl)"
+#mac_address="$(cat /sys/class/net/"$interface"/address)"
+
+name="$(dmidecode -s system-serial-number)"
+#wifi_name="TUMLAB-$(echo "$name" | cut -c 9-12)"
+add_parameters mac_address "\"$name\"" 6
+
+# licence_number="bt""$id_batch""p""$id_project""d""$id_deparment""t""$id_town""i""$id_institution""b""$id_branch""-""$name""-""$ramdom_licence_number"""
+# echo "licence_number= $licence_number"
+# jq '.tumlab.'"information"' += [{"license":"'"$licence_number"'"}]' $path_json > tmp.json && rm $path_json && jq '.' tmp.json > $path_json && rm tmp.json
+
+echo "Digite el numero de la licencia del Tumlab: "
+read -r license_sync
+add_parameters license_sync "\"$license_sync\"" 7
+
+echo "Digite el numero de la licencia del LMS: "
+read -r license_perpetual_lms
+add_parameters license_perpetual_lms "\"$license_perpetual_lms\"" 8
+
+cat "$path_json"
+echo "------------Fin de parametrizacion de informacion del tumlab------------ "echo ""
+echo ""
+echo "------------Parametrizacion de nombre tumlab------------ "
+echo "server name: $name"
+hostnamectl set-hostname "$name"    
+#old_line="$(sed '16q;d' $path_config_promtail)"
+#sed -i.bak "16 s/$old_line/          mac_address: $name/" $path_config_promtail
+
+sudo sed -i.bak "2 s/127.0.0.1 aulasquindiosrv/127.0.0.1 $name/" /etc/hosts
+#sudo sed -i.bak "4 s/listen-address=127.0.0.1/#listen-address=127.0.0.1/" /etc/dnsmasq.conf
+#sudo sed -i.bak "4 s/ssid=TUMLAB/ssid=$wifi_name/" /etc/hostapd/hostapd.conf
+echo "------------Fin de parametrizacion de nombre tumlab------------"
