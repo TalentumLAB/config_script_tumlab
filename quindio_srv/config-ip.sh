0f858b5311a9c3c2dc22437c87b27f7491210c14
Jose Ramiro Ramirez Vargas <117289972+espserver@users.noreply.github.com>
2023-10-02 10:37:49 -0500

    Add config ip files

--- quindio_srv/config-ip.sh
+++ quindio_srv/config-ip.sh
@@ -4,10 +4,9 @@ path_hosts="/etc/hosts"
 
 path_netplan="/etc/netplan/00-installer-config.yaml"
 echo "$path_hosts"
-name="$(hostname)"
 
-rm "$path_hosts"
-cp ./hosts "path_hosts"
+
+cat hosts > "path_hosts"
 
 rm "$path_netplan"
 cp ./00-installer-config.yaml "$path_netplan"
